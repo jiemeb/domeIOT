b7f506f60a6f3bbfc140c061d3ade50dea3b612f
jiemeb <49233521+jiemeb@users.noreply.github.com>
2019-04-11 10:51:40 +0200

    Update script_command.sh

--- outil/domoticz/script_command.sh
+++ outil/domoticz/script_command.sh
@@ -4,29 +4,9 @@ echo  "TEST script"
  
 if [ "$1" == "0" ]
 then
-/bin/echo -e "\x0$2\x0\x2\x$3" >>/tmp/rfm12b_in.fifo 
+curl  http://192.168.0.10/order?order=030$2020$3  # Size of mess, Add, Order, Value  
 else
-/bin/echo -e "\x0$2\x0\x3\x$3" >>/tmp/rfm12b_in.fifo 
+curl  http://192.168.0.10/order?order=030$2030$3  
+#/bin/echo -e "\x0$2\x0\x3\x$3" >>/tmp/rfm12b_in.fifo 
 fi
 exit 0
-
-# if(devicechanged['2.2']=='On')then
-#--	os.execute([[/bin/echo -e "\x02\x0\x3\x2" >/tmp/rfm12b_in.fifo ]])  -- Set Bit card 30 bit 0
-# --  end
-
-
-#--   if(devicechanged['2.2']=='Off')then
-#--	os.execute([[/bin/echo -e "\x02\x0\x2\x2" >/tmp/rfm12b_in.fifo ]] )  -- Reset Bit card 30 bit 0
-#--   end
-
-#--   if(devicechanged['2.1']=='On')then
-#--	os.execute([[/bin/echo -e "\x02\x0\x3\x1" >/tmp/rfm12b_in.fifo ]])  -- Set Bit card 30 bit 0
-#--   end
-
-#--   if(devicechanged['2.1']=='Off')then
-#--	os.execute([[/bin/echo -e "\x02\x0\x2\x1" >/tmp/rfm12b_in.fifo ]])  -- Reset Bit card 30 bit 
- 
- 
- 
- 
-#return commandArray
