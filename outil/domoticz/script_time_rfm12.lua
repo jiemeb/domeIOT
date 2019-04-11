ae137200448155962d6d719579dc03e951cd6a20
jiemeb <49233521+jiemeb@users.noreply.github.com>
2019-04-11 10:50:40 +0200

    Update script_time_rfm12.lua
    Call scrip shell in background to avoid time-out lua

--- outil/domoticz/script_time_rfm12.lua
+++ outil/domoticz/script_time_rfm12.lua
@@ -1,4 +1,3 @@
-
 commandArray = {}
 
 --t = os.date('*t')
@@ -14,30 +13,3 @@ end
 --file:close()
  
 return commandArray
-root@jm-BT3-Pro:/home/domoticz/domoticz/scripts/lua# cat script_time.sh 
-#!/bin/bash
-set -x
-while read card bit event
-do
-card=0x$card
-bit=0x$bit
-echo $card $bit event
-if [ $event -eq  0 ]
-then 
-continu
-fi
-if [ "$bit" = "0xFF"  ]
- then
-chaineCurl=`printf 'curl http://192.168.0.10/order?order=01%02X0F' $card`
-echo $chaineCurl
-$chaineCurl
-else
-chaineCurl=`printf 'curl http://192.168.0.10/order?order=02%02X05%02X' $card $bit`
-echo $chaineCurl
-$chaineCurl
-#curl http://192.168.0.10/order?order=02$card05$bit
-fi
-sleep 1
-done < /etc/event_domoticz.ini
- 
-exit 0
