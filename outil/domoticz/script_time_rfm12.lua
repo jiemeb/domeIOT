9db8d51f9197ec87c4dbdc03ab5c6008ad92d58d
jiemeb <49233521+jiemeb@users.noreply.github.com>
2019-04-11 10:47:43 +0200

    Update script_time_rfm12.lua
    lancement 2 minutes. appels gateway at 192.168.0.10

--- outil/domoticz/script_time_rfm12.lua
+++ outil/domoticz/script_time_rfm12.lua
@@ -1,44 +1,43 @@
 
 commandArray = {}
 
---local a ,b,c
-local command
-local file = io.open("/etc/event_domoticz.ini", "r")
- tbllines = {}
-local i = 0
---  while true 
---do
---  a,b,c=file:read("*n","*n","*n")
---  if a == nil then break end
---  print( a..' '..b..' '..c..' ')
-while true do
-        ligne = file:read("*l")
-        if ligne == nil then break end
- --       print (ligne)
-t={}
-i=0
-for token in string.gmatch(ligne, "[^%s]+") do
-        t[i]=tonumber(token,16)
-        i=i+1
-end
-if t[1] < 255 
-	then
-	command = [[/bin/echo -e "\x]]..string.format("%x",t[0])..[[\x0\x5\x]]..string.format("%x",t[1])..[[" >>/tmp/rfm12b_in.fifo ]]
-	print (command )
-	else
-	command = [[/bin/echo -e "\x]]..string.format("%x",t[0])..[[\x0\xF" >>/tmp/rfm12b_in.fifo ]]
-	print (command )
-	end
+--t = os.date('*t')
+nowTable = os.date('*t')
+nowMin = nowTable.min
+print ( nowMin )
+if ( nowMin %2 == 0) 
+then
+command = [[/home/domoticz/domoticz/scripts/lua/script_time.sh & ]]
 os.execute(command)
+print (commande)
 end
-file:close()
-
-
---os.execute([[/bin/echo -e "\x2\x0\x5\x2" >/tmp/rfm12b_in.fifo ]] );  -- Read cardd 30 bit 0
---os.execute([[/bin/echo -e "\x2\x0\x5\x1" >/tmp/rfm12b_in.fifo ]] )  -- Read cardd 30 bit 1
---os.execute([[/bin/echo -e "\x2\x0\xF" >/tmp/rfm12b_in.fifo ]] )  -- Read cardd 30 
---	os.execute([[/bin/echo -e "\x1E\x0\x5\x2" >/tmp/rfm12b_in.fifo ]] );  -- Read cardd 30 bit 0
---	os.execute([[/bin/echo -e "\x1E\x0\x5\x1" >/tmp/rfm12b_in.fifo ]] )  -- Read cardd 30 bit 1
---	os.execute([[/bin/echo -e "\x1E\x0\xF" >/tmp/rfm12b_in.fifo ]] )  -- Read cardd 30 
+--file:close()
  
 return commandArray
+root@jm-BT3-Pro:/home/domoticz/domoticz/scripts/lua# cat script_time.sh 
+#!/bin/bash
+set -x
+while read card bit event
+do
+card=0x$card
+bit=0x$bit
+echo $card $bit event
+if [ $event -eq  0 ]
+then 
+continu
+fi
+if [ "$bit" = "0xFF"  ]
+ then
+chaineCurl=`printf 'curl http://192.168.0.10/order?order=01%02X0F' $card`
+echo $chaineCurl
+$chaineCurl
+else
+chaineCurl=`printf 'curl http://192.168.0.10/order?order=02%02X05%02X' $card $bit`
+echo $chaineCurl
+$chaineCurl
+#curl http://192.168.0.10/order?order=02$card05$bit
+fi
+sleep 1
+done < /etc/event_domoticz.ini
+ 
+exit 0
